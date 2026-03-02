9d7b2e0e29deb87b87d614279f68dff04d71bc05
satriazoid <satriaalfa77@gmail.com>
2026-03-02 16:17:36 +0700

    rename file and add project

---
+++ add-folder-automation.ps1
@@ -0,0 +1,13 @@
+New-Item -ItemType Directory `  
+"Semester-6\Mk-Rekayasa-Perangkat-Lunak",  
+"Semester-6\Mk-Kerja-Praktek",  
+"Semester-6\Mk-Teknologi-internet-of-things",  
+"Semester-6\Mk-Pemograman-2",
+"Semester-6\Mk-Basis-data", 
+"Semester-6\Mk-Mobile-programming", 
+"Semester-6\Mk-Sistem-pendukung-keputusan", 
+"Semester-6\Mk-teknik-kompilasi"
+
+# ini contoh dari penambahan secara langsung
+# Semester-6 adalah penyimpanan directory yang anda ingin buat
+# sebagai contoh directory cd c:\Kuliah\, maka lanjut ke peintah di atas 
