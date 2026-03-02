a75aad0a659a5227edb2e78c386720b63f590af1
satriazoid <satriaalfa77@gmail.com>
2026-03-02 16:43:04 +0700

    edit

--- add-folder-automation.ps1
+++ add-folder-automation.ps1
@@ -1,12 +1,4 @@
-New-Item -ItemType Directory `  
-"Semester-6\Mk-Rekayasa-Perangkat-Lunak",  
-"Semester-6\Mk-Kerja-Praktek",  
-"Semester-6\Mk-Teknologi-internet-of-things",  
-"Semester-6\Mk-Pemograman-2",
-"Semester-6\Mk-Basis-data", 
-"Semester-6\Mk-Mobile-programming", 
-"Semester-6\Mk-Sistem-pendukung-keputusan", 
-"Semester-6\Mk-teknik-kompilasi"
+New-Item -ItemType Directory `"Semester-6\Mk-Rekayasa-Perangkat-Lunak", "Semester-6\Mk-Kerja-Praktek", "Semester-6\Mk-Teknologi-internet-of-things", "Semester-6\Mk-Pemograman-2", "Semester-6\Mk-Basis-data", "Semester-6\Mk-Mobile-programming", "Semester-6\Mk-Sistem-pendukung-keputusan", "Semester-6\Mk-teknik-kompilasi"
 
 # ini contoh dari penambahan secara langsung
 # Semester-6 adalah penyimpanan directory yang anda ingin buat
